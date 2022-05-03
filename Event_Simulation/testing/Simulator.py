18ce9f72681502c7d105f1bdefba9d9cc6d7e6b8
MatiasNegro <67411732+MatiasNegro@users.noreply.github.com>
2022-05-03 18:24:44 +0200

    Request first function (NOT WORKING)

--- Event_Simulation/testing/Simulator.py
+++ Event_Simulation/testing/Simulator.py
@@ -172,6 +172,6 @@ def main():
     for i in f_r_l:
         el_tree = ET.ElementTree(i)
         name = el_tree.find("Event").find("Name").text + el_tree.find("Event").find("StartTime").find("Date").text + '.xml'
-        el_tree.write('Event_Simulation/' + name, encoding = 'utf-8')
+        el_tree.write('Event_Simulation/testing/Result_Of_Simulation/' + name, encoding = 'utf-8')
   
 main()
---
+++ Event_Simulation/testing/uploader.py
@@ -0,0 +1,14 @@
+import requests
+import glob
+import os
+
+os.chdir("Event_Simulation/testing/Result_Of_Simulation")
+url = ''
+
+files = []
+for file in glob.glob("*.xml"):
+    files.append({file : open(file, 'r')})
+
+print(dict(files))
+
+#response = requests.post(url= url, headers='file-name', files=dict(files))
