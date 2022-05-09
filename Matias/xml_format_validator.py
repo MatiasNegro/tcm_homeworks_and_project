dda855388789a68c3f6eaaf8315c01a28f43488e
MatiasNegro <67411732+MatiasNegro@users.noreply.github.com>
2022-05-09 17:44:34 +0200

    Format Validator

--- Matias/xml_format_validator.py
+++ Matias/xml_format_validator.py
@@ -1,4 +1,24 @@
-import xml.etree.ElementTree as ET
+from lxml import etree
 
-def xml_validator(xml_doc):
-    pass
+def format_validator(root):
+    source_file = root
+    schema_file = 'schema.xsd'
+    flag = True
+
+    with open(schema_file) as f_schema:
+
+        schema_doc = etree.parse(f_schema)
+        schema = etree.XMLSchema(schema_doc)
+        parser = etree.XMLParser(schema = schema)
+
+        with root as f_source:
+            try:
+                doc = etree.parse(f_source, parser)
+            except etree.XMLSyntaxError as e:
+                # this exception is thrown on schema validation error
+                print(e)
+                flag = False
+    
+    return flag
+            
+    
