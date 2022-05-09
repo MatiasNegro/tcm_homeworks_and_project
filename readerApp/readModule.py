04c5ee68c72810bb9312e3f69c7b5304cd892cbe
DaniGreek <d.greco6@studenti.unibg.it>
2022-05-09 10:27:59 +0200

    bubu

--- readerApp/readModule.py
+++ readerApp/readModule.py
@@ -8,112 +8,109 @@ import requests
 # global
 url = 'https://x4d1kgdj83.execute-api.us-east-1.amazonaws.com/default/download'
 
-def readerFiles(identityToken):
-    def info(massage):
-        showinfo(
-            title = 'Result',
-            message = massage
-        )
+def info(massage):
+    showinfo(
+        title = 'Result',
+        message = massage
+    )
 
-    try:
+try:
 
-        def showFiles():
+    def showFiles():
+        headers = {
+            'mod':'read'
+        }
+        r = requests.get(url, headers=headers)
+        content = r.content
+        body = content.decode('utf-8')
+        nameList = body.replace('partite/','')
+        return nameList
+
+    def downloadFile():
+        searchedFile = fileSearched_textbox.get("1.0", END + '-1c')
+        searchedFile = searchedFile.replace('\n', '')
+        if(searchedFile != ''):
             headers = {
-                'Authorization':identityToken,
-                'mod':'read'
+                'filename':searchedFile,
+                'mod':'download'
             }
             r = requests.get(url, headers=headers)
-            content = r.content
-            body = content.decode('utf-8')
-            nameList = body.replace('partite/','')
-            return nameList
+            file = r.content
+            file = file.decode('utf-8')
+            if('ResponseMetadata' in file):
+                info('Error 404: file not found.')
+            else: 
+                destinationUrl = 'downloads/' + fileSearched_textbox.get("1.0", END + '-1c') + '.json'
+                f = open(destinationUrl, 'w+')
+                f.write(file)
+                f.close()
+                info('File downloaded succesfully, check downloads folder.')
+            fileSearched_textbox.delete('1.0', END)
 
-        def downloadFile():
-            searchedFile = fileSearched_textbox.get("1.0", END + '-1c')
-            searchedFile = searchedFile.replace('\n', '')
-            if(searchedFile != ''):
-                headers = {
-                    'Authorization':identityToken,
-                    'filename':searchedFile,
-                    'mod':'download'
-                }
-                r = requests.get(url, headers=headers)
-                file = r.content
-                file = file.decode('utf-8')
-                if('ResponseMetadata' in file):
-                    info('Error 404: file not found.')
-                else: 
-                    destinationUrl = 'downloads/' + fileSearched_textbox.get("1.0", END + '-1c') + '.json'
-                    f = open(destinationUrl, 'w+')
-                    f.write(file)
-                    f.close()
-                    info('File downloaded succesfully, check downloads folder.')
-                fileSearched_textbox.delete('1.0', END)
+    # root window
+    root = Tk()
+    photo = PhotoImage(file="img/icon.png")
+    root.iconphoto(False, photo)
+    root.title('Download files')
+    root.geometry('500x500')
+    root.resizable(True, True)
+    frame = Frame(root)
+    frame.pack(fill = BOTH, expand = True, padx = 10, pady = 20)
 
-        # root window
-        root = Tk()
-        photo = PhotoImage(file="img/icon.png")
-        root.iconphoto(False, photo)
-        root.title('Download files')
-        root.geometry('500x500')
-        root.resizable(True, True)
-        frame = Frame(root)
-        frame.pack(fill = BOTH, expand = True, padx = 10, pady = 20)
+    fileNames = showFiles()
 
-        fileNames = showFiles()
+    # label title
+    labelFileName = ttk.Label(
+        frame,
+        text = 'Files in the database:',
+        font = ('calibri', 13)
+    )
+    labelFileName.pack(
+        pady = 5,
+        ipadx = 5,
+        side = 'top'
+    )
+    # label with all the files in the bucket
+    labelFileName = ttk.Label(
+        frame,
+        text = fileNames,
+        font = ('calibri', 11)
+    )
+    labelFileName.pack(
+        ipadx = 5,
+        side = 'top'
+    )
 
-        # label title
-        labelFileName = ttk.Label(
-            frame,
-            text = 'Files in the bucket:',
-            font = ('calibri', 13)
-        )
-        labelFileName.pack(
-            pady = 5,
-            ipadx = 5,
-            side = 'top'
-        )
-        # label with all the files in the bucket
-        labelFileName = ttk.Label(
-            frame,
-            text = fileNames,
-            font = ('calibri', 11)
-        )
-        labelFileName.pack(
-            ipadx = 5,
-            side = 'top'
-        )
+    downloadFrame = Frame(frame)
+    downloadFrame.pack(fill = BOTH, expand = True)
 
-        downloadFrame = Frame(frame)
-        downloadFrame.pack(fill = BOTH, expand = True)
+    # button that downloads file
+    buttonSend = ttk.Button(
+        frame,
+        text = 'Download',
+        command = downloadFile
+    )
+    buttonSend.pack(
+        ipadx = 5,
+        ipady = 5,
+        side = 'left'
+    )
+    # file name textbox
+    fileSearched_textbox = Text(
+        frame,
+        height = 1,
+        background = "white",
+        foreground = "black",
+        font = ('calibri', 11)
+    )
+    fileSearched_textbox.pack(
+        pady = 5,
+        fill = 'x',
+        side = 'right'
+    )
 
-        # button that downloads file
-        buttonSend = ttk.Button(
-            frame,
-            text = 'Download',
-            command = downloadFile
-        )
-        buttonSend.pack(
-            ipadx = 5,
-            ipady = 5,
-            side = 'left'
-        )
-        # file name textbox
-        fileSearched_textbox = Text(
-            frame,
-            height = 1,
-            background = "white",
-            foreground = "black",
-            font = ('calibri', 11)
-        )
-        fileSearched_textbox.pack(
-            pady = 5,
-            fill = 'x',
-            side = 'right'
-        )
+    root.mainloop()
 
-        root.mainloop()
-
-    except BaseException as error:
-        showinfo(title='Error', message="Sorry, there was an error :/")
-        root.destroy()
+except BaseException as error:
+    showinfo(title='Error', message="Sorry, there was an error :/")
+    root.destroy()
