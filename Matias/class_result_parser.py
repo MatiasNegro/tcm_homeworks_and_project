0d96b4dd89dc75e8cbb286bbe9b5b53cd3388fd4
MatiasNegro <67411732+MatiasNegro@users.noreply.github.com>
2022-05-10 11:49:42 +0200

    Bug fix for xml_parser

--- Matias/class_result_parser.py
+++ Matias/class_result_parser.py
@@ -1,17 +1,18 @@
 import xml.etree.ElementTree as ET
 import copy
 
-def class_result_parser(xml_doc): 
+def class_result_parser(root): 
     '''
     Prende in ingresso una root del documento xml e restituisce le classi dentro un unico tag <ClassResults> per garantire la compatibilità con dynamodb ed evitare la sovrascrittura dei <ClassResult>
     '''
     class_results_base = ET.fromstring('<ClassResults></ClassResults>')
     result_list = ET.fromstring('<ResultList></ResultList>')
-    root = xml_doc.getroot()
-    event = root.find("Event")
-    c_r_s = copy.deepcopy(class_results_base) 
-    result_list.append(event)
+    c_r_s = copy.deepcopy(class_results_base)
+    r_l = copy.deepcopy(result_list)
+    event = copy.deepcopy(ET.ElementTree(root.find("Event"))).getroot()
+    r_l.append(event)
     for i in root.findall("ClassResult"):
         c_r_s.append(i)
-    result_list.append(c_r_s)
-    return result_list
+    r_l.append(c_r_s)
+    return r_l
+
