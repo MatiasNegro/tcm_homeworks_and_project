b9d1ec0d618aff507b93b62146a38b3da4ef4b4e
DaniGreek <d.greco6@studenti.unibg.it>
2022-05-10 19:44:50 +0200

    yes

--- lambda/readerFunction.py
+++ lambda/readerFunction.py
@@ -24,86 +24,96 @@ def lambda_handler(event, context):
     resource = resource.replace('/','')
     response['body'] = ''
     
-    try:
-        table = dynamodb.Table('DBresults')
+
+    table = dynamodb.Table('DBresults')
+    
+    if(resource == 'list_races'):
         
-        if(resource == 'list_races'):
-            dict = {}
-            r = ddb_client.scan(
-                TableName='DBresults',
-                AttributesToGet=[
-                    'event',
-                    'Event'
-                ],
-            )
-            j = 0
-            for i in r['Items']:
-                dict[j] = {
-                    'race_name' : i['Event']['M']['Name']['S'],
-                    'race_date' : i['Event']['M']['EndTime']['M']['Date']['S'],
-                    'race_id' : i['event']['S'],
-                }
-                j += 1
-            dict_json = json.dumps(dict, indent=4)
-            dict_json = dict_json.split(', "ResponseMetadata":')
-            dict_json = dict_json[0]
-            response['body'] = dict_json
-            
-        elif(resource == 'download'):
-            #Getting the key
-            key = event["headers"]["filename"]
-            #Query
-            item = table.get_item(Key={'event': key})
-            #Converting the item <dict> to json <string>
-            item_json = json.dumps(item)
-            item_json = item_json.split(', "ResponseMetadata":')
-            item_json = item_json[0] + "}"
-            #Item return when the query has been possile (statusCode:200)
-            response['body'] = item_json
-            
-        elif(resource == 'list_classes'):
-            dict = {}
-            id = event["queryStringParameters"]["id"]
-            r = ddb_client.scan(
-                TableName='DBresults',
-                AttributesToGet=[
-                    'event',
-                    'ClassResults'
-                ],
-            )
-            j = 0
-            for i in r['Items']:
-                if (i['event']['S'] == id):
-                    dict = i['ClassResults']
-            dict_json = json.dumps(dict, indent=4)
-            dict_json = dict_json.split(', "ResponseMetadata":')
-            dict_json = dict_json[0]
-            response['body'] = dict_json
-            
-        elif(resource == 'results'):
-            dict = {}
-            id = event["queryStringParameters"]["id"]
-            r = ddb_client.scan(
-                TableName='DBresults',
-                AttributesToGet=[
-                    'event',
-                    'ClassResults'
-                ],
-            )
-            j = 0
-            for i in r['Items']:
-                if (i['event']['S'] == id):
-                    dict = i['ClassResults']
-                    
-            '''dict_json = json.dumps(dict, indent=4)
-            dict_json = dict_json.split(', "ResponseMetadata":')
-            dict_json = dict_json[0]
-            response['body'] = dict_json'''
-            
-        else:
-            response['body'] = 'Error 404: resource not found'
+        dict = {}
+        r = ddb_client.scan(
+            TableName='DBresults',
+            AttributesToGet=[
+                'event',
+                'Event'
+            ],
+        )
+        j = 0
+        for i in r['Items']:
+            dict[j] = {
+                'race_name' : i['Event']['M']['Name']['S'],
+                'race_date' : i['Event']['M']['EndTime']['M']['Date']['S'],
+                'race_id' : i['event']['S'],
+            }
+            j += 1
+        dict_json = json.dumps(dict, indent=4)
+        dict_json = dict_json.split(', "ResponseMetadata":')
+        dict_json = dict_json[0]
+        response['body'] = dict_json
         
-    except Exception as e:
-        raise IOError(e)
+        
+    elif(resource == 'download'):
+        
+        #Getting the key
+        key = event["headers"]["filename"]
+        #Query
+        item = table.get_item(Key={'event': key})
+        #Converting the item <dict> to json <string>
+        item_json = json.dumps(item)
+        item_json = item_json.split(', "ResponseMetadata":')
+        item_json = item_json[0] + "}"
+        #Item return when the query has been possile (statusCode:200)
+        response['body'] = item_json
+        
+        
+    elif(resource == 'list_classes'):
+        
+        dict = {}
+        id = event["queryStringParameters"]["id"]
+        r = ddb_client.scan(
+            TableName='DBresults',
+            AttributesToGet=[
+                'event',
+                'ClassResults'
+            ],
+        )
+        j = 0
+        for i in r['Items']:
+            if (i['event']['S'] == id):
+                dict = i['ClassResults']
+        dict_json = json.dumps(dict, indent=4)
+        dict_json = dict_json.split(', "ResponseMetadata":')
+        dict_json = dict_json[0]
+        response['body'] = dict_json
+        
+        
+    elif(resource == 'results'):
+        
+        dict = {}
+        id = event["queryStringParameters"]["id"]
+        cl = event["queryStringParameters"]["class"]
+        r = ddb_client.scan(
+            TableName='DBresults',
+            AttributesToGet=[
+                'event',
+                'ClassResults'
+            ],
+        )
+        j = 0
+        for i in r['Items']:
+            if (i['event']['S'] == id):
+                for k in i['ClassResults']['M']:
+                    for l in i['ClassResults']['M'][k]['L']:
+                        print(l['M']['Class']['M']['Id']['S'])
+                
+                
+        '''dict_json = json.dumps(dict, indent=4)
+        dict_json = dict_json.split(', "ResponseMetadata":')
+        dict_json = dict_json[0]
+        response['body'] = dict_json'''
+        
+        
+    else:
+        response['body'] = 'Error 404: resource not found'
+        
             
     return response
